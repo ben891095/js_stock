650ff2065a6f6ca298750baa2698d787a54b2a43
unknown <6545@citegroup.tw>
2017-09-29 18:07:14 +0800

    2017/09/29

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var stock_list = ["1101","3231","2330"]; 
+
+
+//$.ajax({
+//    url:url,
+//    dataType:'jsonp',
+//}).done(function (data) {
+//    alert(data);
+//    console.log(JSON.stringify(data));
+//});
