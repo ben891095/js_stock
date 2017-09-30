13544b05a6ecb567e37260c9617b07fb1332001e
unknown <6545@citegroup.tw>
2017-09-30 16:23:49 +0800

    2017/09/30

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-
+eudora chen
 var stock_list = ["1101","3231","2330"]; 
+var url = 'https://tw.quote.finance.yahoo.net/quote/q?type=tick&sym=1457';
 
 
-//$.ajax({
-//    url:url,
-//    dataType:'jsonp',
-//}).done(function (data) {
-//    alert(data);
-//    console.log(JSON.stringify(data));
-//});
+$.ajax({
+    url:url,
+    dataType:'jsonp',
+    jsonp: 'callback',
+    jsonpCallback: 'jsonp_callback'
+});
+
+function jsonp_callback(data) {
+    console.log(data)
+}
